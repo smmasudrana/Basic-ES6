56117a4e1be7bdf95fe7c4bf9d15fc1333d94891
smmasudrana <smmasudrana62@gmail.com>
2025-08-30 15:33:30 +0600

    add tamplate string

---
+++ tamplate-string.js
@@ -0,0 +1,21 @@
+const poem =`Amm pata jora jora
+marbo chabuk chorbe ghora 
+ore bubu ghure dara 
+asche amar pagla ghora
+`
+console.log(poem);
+
+
+//------------
+function sum (num1, num2){
+    const result = num1 + num2;
+    const output = `sum of ${num1} and ${num2} is equal to ${result}`
+    console.log(output);
+}
+
+sum(10, 20)
+
+//---------------
+const price = 100;
+const discountPrice =`Discounted price is after 20% discount ${price * 0.8}`
+console.log(discountPrice);
