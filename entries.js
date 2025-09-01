8cc918f8e4842d0e88770bc9636d0e64c4d898fd
smmasudrana <smmasudrana62@gmail.com>
2025-09-01 11:09:06 +0600

    add entries

---
+++ entries.js
@@ -0,0 +1,14 @@
+const employee = {
+    name: 'raj rani',
+    designation: 'QA',
+    salary: 20000,
+    experience: 2,
+}
+
+const keys =Object.keys(employee);
+const values = Object.values(employee);
+const entries = Object.entries(employee)
+
+console.log(keys);
+console.log(values);
+console.log(entries);
